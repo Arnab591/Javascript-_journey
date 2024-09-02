bf2bfaf61e23e29ea10e05506757d014f3a237be
Arnab591 <133198956+Arnab591@users.noreply.github.com>
2024-09-02 19:21:10 +0000

    datatypes conversion

---
+++ 01_basic/typeconversion.js
@@ -0,0 +1,14 @@
+let number = 33
+console.log(number)
+numberinStr = String (number);
+console.log(numberinStr);
+console.log(typeof numberinStr)
+// String() or Number() are some method to convert the datatypes
+let numberstr = "33abc"
+// 33abc string to number converts but this will give NaN --> not a number
+numberinInt = Number (numberstr)
+console.log(numberinInt);
+console.log(typeof numberinInt) 
+// the following string not convert to number but the type shows it will be a number
+// same we can do with boolean also
+
